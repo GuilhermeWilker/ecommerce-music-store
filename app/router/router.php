20f2ea6ba35886085b45c92c0da26e48155e1dfc
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-07-10 16:13:34 -0300

    feat: adding dynamic routing

--- app/router/router.php
+++ app/router/router.php
@@ -8,10 +8,23 @@ function routes(): array
 function ifTheUriMatchInArrayRoutes(string $uri, array $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        return [];
+        return [$uri => $routes[$uri]];
     }
 
     return [];
+}
+
+function ifRegularExpressionMatchArrayRoutes($uri, $routes)
+{
+    return array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
 }
 
 function router()
@@ -21,4 +34,10 @@ function router()
     $routes = routes();
 
     $matchedUri = ifTheUriMatchInArrayRoutes($uri, $routes);
+    if (empty($matchedUri)) {
+        $matchedUri = ifRegularExpressionMatchArrayRoutes($uri, $routes);
+    }
+
+    var_dump($matchedUri);
+    exit;
 }
--- app/router/web.php
+++ app/router/web.php
@@ -3,4 +3,5 @@
 return [
         '/' => 'Home@index',
         '/pagamento' => 'Pagamento@index',
+        '/pagamento/[0-9]+' => 'Pagamento@index',
         ];

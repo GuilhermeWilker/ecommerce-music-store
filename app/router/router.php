93975b2bc0d2937f19fe926c7b799ba929c5af74
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-07-10 15:15:39 -0300

    feat: creating the routing system

--- app/router/router.php
+++ app/router/router.php
@@ -1,1 +1,24 @@
 <?php
+
+function routes(): array
+{
+    return require 'web.php';
+}
+
+function ifTheUriMatchInArrayRoutes(string $uri, array $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        return [];
+    }
+
+    return [];
+}
+
+function router()
+{
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $routes = routes();
+
+    $matchedUri = ifTheUriMatchInArrayRoutes($uri, $routes);
+}
---
+++ app/router/web.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+        '/' => 'Home@index',
+        '/pagamento' => 'Pagamento@index',
+        ];
